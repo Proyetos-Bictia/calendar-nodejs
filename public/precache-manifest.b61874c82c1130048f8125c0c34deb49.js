997cf5dc65c3887ad05b82e08836fb7ecd393476
Camilo Davila <camilo7davila@gmail.com>
2020-08-08 18:41:15 -0500

    calendar app

---
+++ public/precache-manifest.b61874c82c1130048f8125c0c34deb49.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "d3843b27f22e8785a7b52a7710c2c4a6",
+    "url": "/index.html"
+  },
+  {
+    "revision": "db59be65bccb75e003de",
+    "url": "/static/css/2.59a0ab78.chunk.css"
+  },
+  {
+    "revision": "412ce0cea3fbb7166bbf",
+    "url": "/static/css/main.6922468d.chunk.css"
+  },
+  {
+    "revision": "db59be65bccb75e003de",
+    "url": "/static/js/2.46c1360f.chunk.js"
+  },
+  {
+    "revision": "fa4b4056bba234820139274405351863",
+    "url": "/static/js/2.46c1360f.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "412ce0cea3fbb7166bbf",
+    "url": "/static/js/main.7aaae453.chunk.js"
+  },
+  {
+    "revision": "7e829d7f4f2411c25d99",
+    "url": "/static/js/runtime-main.8c2230a5.js"
+  }
+]);
